bab1a5b090b01252ef71fb1e19a480e089bceb98
 <mishgsn90@gmail.com>
2026-03-02 23:46:04 +0300

    Add main.py/run.py. Starting with error

--- backend/app/routes/product.py
+++ backend/app/routes/product.py
@@ -10,16 +10,16 @@ router = APIRouter(
 )
 
 @router.get("", response_model=ProductListResponse, status_code=status.HTTP_200_OK)
-def get_products(db:Session = Depend(get_db)):
+def get_products(db:Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_all_products()
 
 @router.get("/{product_id}", response_model=ProductResponse, status_code=status.HTTP_200_OK)
-def get_product_by_id(product_id: int, db: Session = Depend(get_db)):
+def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_product_by_id(product_id)
 
 @router.get("/category/{category_id}", response_model=ProductListResponse, status_code=status.HTTP_200_OK)
-def get_products_by_category(category_id: int, db: Session = Depend(get_db)):
+def get_products_by_category(category_id: int, db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_products_by_category(category_id)
